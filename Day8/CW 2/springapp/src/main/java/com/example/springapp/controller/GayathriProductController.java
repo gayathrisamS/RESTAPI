f031ebda298355351237beb645e982fb1f6333ce
GAYATHRI S <124783400+gayathrisamS@users.noreply.github.com>
2024-04-22 12:26:58 +0530

    Add files via upload

---
+++ Day8/CW 2/springapp/src/main/java/com/example/springapp/controller/GayathriProductController.java
@@ -0,0 +1,57 @@
+package com.example.springapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import com.example.springapp.model.GayathriProduct;
+import com.example.springapp.service.GayathriProductService;
+
+@RestController
+public class GayathriProductController {
+    @Autowired
+    private GayathriProductService ser;
+
+    @PostMapping("/product")
+    public ResponseEntity<GayathriProduct> post(@RequestBody GayathriProduct person) {
+        if (ser.post(person)) {
+            return new ResponseEntity<>(person, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/product/distinct/startwith/{name}")
+    public ResponseEntity<List<GayathriProduct>> getAll(@PathVariable("name") String name) {
+        List<GayathriProduct> li = ser.start(name);
+        if (li.size() > 0) {
+            return new ResponseEntity<>(li, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("product/distict/endswith/{name}")
+    public ResponseEntity<List<GayathriProduct>> getbyAge(@PathVariable("name") String name) {
+        List<GayathriProduct> li = ser.end(name);
+        if (li.size() > 0) {
+            return new ResponseEntity<>(li, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @GetMapping("/product/sortBy/price")
+    public ResponseEntity<List<GayathriProduct>> getbyge() {
+        List<GayathriProduct> li = ser.sort();
+        if (li.size() > 0) {
+            return new ResponseEntity<>(li, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ Day8/CW 3/springapp/src/main/java/com/example/springapp/controller/GayathriStudentController.java
@@ -0,0 +1,48 @@
+package com.example.springapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import com.example.springapp.model.GayathriStudent;
+import com.example.springapp.service.GayathriStudentService;
+
+@RestController
+public class GayathriStudentController {
+    @Autowired
+    private GayathriStudentService studentService;
+
+    @PostMapping("/api/student")
+    public ResponseEntity<GayathriStudent> post(@RequestBody GayathriStudent student) {
+        if (studentService.post(student)) {
+            return new ResponseEntity<>(student, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/api/student/markGreaterThan/{marks}")
+    public ResponseEntity<List<GayathriStudent>> getGreater(@PathVariable int marks) {
+        List<GayathriStudent> list = studentService.getGreater(marks);
+        if (list.size() > 0) {
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/api/student/markLessThan/{marks}")
+    public ResponseEntity<List<GayathriStudent>> getLesser(@PathVariable int marks) {
+        List<GayathriStudent> list = studentService.getLesser(marks);
+        if (list.size() > 0) {
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
